40f964412ae60a814f43d3e073a08a66a4b9e8ba
Carlos Doria <carlosdoria953@gmail.com>
2020-10-11 23:00:13 -0300

    Imported and configured 'react-dnd' and 'react-dnd-html5-backend' in the 'index.js' and 'Card/index.jsx'.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,25 @@
+import { useDrag } from 'react-dnd';
+
 import * as S from './styles';
 
-const Card = ({ data }) => (
-  <S.Wrapper>
-    <S.Header>
-      {data.labels.map(label => <S.Label key={label} color={label}/>)}
-    </S.Header>
-    <S.P>
-      {data.content}
-    </S.P>
-    {data.user && <S.Image src='https://api.adorable.io/avatars/40/test.png' alt='' />}
-  </S.Wrapper>
-)
+export default function Card ({ data }) {
 
-export default Card
+  const [{ isDragging }, dragRef] = useDrag({
+    item: {type: 'CARD'},
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    })
+  });
+
+  return (
+    <S.Wrapper ref={dragRef}>
+      <S.Header>
+        {data.labels.map(label => <S.Label key={label} color={label}/>)}
+      </S.Header>
+      <S.P>
+        {data.content}
+      </S.P>
+      {data.user && <S.Image src='https://api.adorable.io/avatars/40/test.png' alt='' />}
+    </S.Wrapper>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
+import { DndProvider } from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
+
 import Header from '../components/Header'
 import Board from '../components/Board'
 
 export default function Home() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
-    </>
+    </DndProvider>
   )
 }
